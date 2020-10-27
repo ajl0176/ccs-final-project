30ae204332cf435b64adb0b2a96e51811fbaf93a
A. James Lindsey <macbookair@MacBooks-MacBook-Air.local>
2020-10-27 11:52:57 -0400

    Installed Apps

--- config/urls.py
+++ config/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('frontend.urls', namespace='frontend')),
+    # allows you to login/out of the browsable api
+    path('api-auth/', include('rest_framework.urls')),
 ]
