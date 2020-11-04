783cbb366daf1e079658c8ad371a4d463e7379ab
A. James Lindsey <macbookair@MacBooks-MacBook-Air.local>
2020-11-04 02:49:07 -0500

    More Forms

--- frontend/static/src/components/MenuForm.js
+++ frontend/static/src/components/MenuForm.js
@@ -1,24 +1,44 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
 // import Cookies from 'js-cookie';
 //
-// class MenuForm extends Component {
-//
-//   constructor(props) {
-//     super(props)
-//
-//     this.state ={
-//       item: '',
-//       price: '',
-//       description: ''.
-//       image: '',
-//     }
-//     this.handleChange = this.handleChange.bind(this)
-//     this.handleSubmit = this.handleSubmit.bind(this)
-//     this.editMenu = this.editMenu.bind(this)
-//   }
-//
-//   handleChange (event){
-//     this.setState({[event.target.name]:event.target.value});
-//   }
-//
-// }
+class MenuForm extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state ={
+      item: '',
+      price: '',
+      description: '',
+      image: '',
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange (event){
+    this.setState({[event.target.name]:event.target.value})
+  }
+
+render(){
+  return(
+  <form className="col-12" onSubmit={(event) => this.props.handleSubmit(event, this.state)}>
+    <div className="form-group">
+      <label htmlFor="item">Item</label>
+      <input type="text" className="form-control" id="item" name="item" value={this.state.item} onChange={this.handleChange}/>
+      <label htmlFor="description">Description</label>
+      <textarea rows='3' type="text" className="form-control" id="body" name="body" value={this.state.body} onChange={this.handleChange}/>
+      <label htmlFor="image">Image</label>
+      <input type="image" scr="img_submit.gif" alt="Submit" width="48" height="48">
+      <label htmlFor="foodCategory">Category</label>
+      <select id="foodCategory" className="form-control"  name="foodCategory" value={this.state.foodcategory} onChange={this.handleChange}/>
+        <option>Menu</option>
+        <option>Make Your Own</option>
+      </select>
+    </div>
+    <button type="submit" className="btn btn-primary">Save</button>
+  </form>
+    )
+  }
+}
+
+export default MenuForm;
---
+++ frontend/static/src/components/OrderForm.js
@@ -0,0 +1,66 @@
+import React, { Component }from 'react';
+// import "../CSS/OrderForm.css";
+
+
+
+
+class OrderForm extends Component {
+constructor(props) {
+  super(props);
+  this.state={
+    }
+  }
+
+
+
+  render() {
+const subtotal = this.props.order.reduce((acc, item)=> {
+  let total = acc + item.price
+    return total
+  }, 0);
+
+
+
+
+  let orderList = this.props.order.map((item, index)=> (
+
+
+        <div className="d-flex" key={index}>
+            <h5 className="col-8">{item.item}</h5>
+            <h5 className="col-2">${item.price}</h5>
+            <button type="button" className="btn col-2" onClick={()=>this.props.deleteOrder(item)}>Delete</button>
+        </div>
+
+
+  ));
+
+
+
+    return(
+      <div className="col">
+        <div className="justify-content-md-center row">
+          <h2 className="main-title">Order Form</h2>
+        </div>
+        <hr/>
+
+      <div className="row justify-content-between">
+          <div className="col-12">{orderList}
+          <hr class="solid"/>
+          </div>
+        </div>
+
+      <div className="row justify-content-center">
+        <div classname="col-6">
+            <div className="subtotal">
+                <h4>Total: ${subtotal}</h4>
+
+                <button onClick={this.props.submitOrder}>Check Out</button>
+
+          </div>
+        </div>
+      </div>
+  </div>
+      )
+    }
+};
+export default OrderForm;
